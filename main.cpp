0c9f28030ffcb6ac5db0be07e41ccab9e6b828d2
Muhammed Rajab <muhammedrajab101@gmail.com>
2024-10-08 19:33:49 +0530

    implemented a more class based approach to the problem. this looks cleaner now. but why the fuck do i suddenly feel like abandoning the project??

--- main.cpp
+++ main.cpp
@@ -30,57 +30,240 @@ struct Pixel
     }
 };
 
-void renderBuffer(Pixel *buffer, int width, int height)
+class Renderer
 {
-    std::ostringstream oss;
-    for (int y = 0; y < height; ++y)
-    {
-        for (int x = 0; x < width; ++x)
-        {
-            Pixel p = buffer[y * width + x];
-            oss << "\033[38;2;" << (unsigned int)p.r << ";" << (unsigned int)p.g << ";" << (unsigned int)p.b << "m" << "██";
-        }
-        oss << "\n";
-    }
-    std::cout << oss.str();
-}
+public:
+    int width;
+    int height;
 
-void fillBuffer(Pixel *buffer, int width, int x, int y, Pixel p)
-{
-    for (int j = 0; j < y; ++j)
-    {
-        for (int i = 0; i < x; ++i)
-        {
-            buffer[j * width + i] = p;
-        }
-    }
-}
+    Pixel *screen = nullptr;
+    Pixel *buffer = nullptr;
 
-void resetBuffer(Pixel *buffer, int width, int height, Pixel p)
-{
-    for (int j = 0; j < height; ++j)
+    // ! CONSTRUCTION/DESTRUCTION
+    Renderer(int width, int height)
     {
-        for (int i = 0; i < width; ++i)
+        this->width = width;
+        this->height = height;
+
+        this->screen = new Pixel[width * height];
+        this->buffer = new Pixel[width * height];
+    }
+
+    ~Renderer()
+    {
+        if (this->screen != nullptr)
         {
-            buffer[j * width + i] = p;
+            delete[] this->screen;
+        }
+        if (this->buffer != nullptr)
+        {
+            delete[] this->buffer;
         }
     }
-}
 
-void putPixel(Pixel *buffer, int width, int x, int y, Pixel color)
-{
-    buffer[y * width + x] = color;
-}
+    // ! RENDERING
+    void render()
+    {
+        std::ostringstream oss;
+        for (int y = 0; y < this->height; ++y)
+        {
+            for (int x = 0; x < this->width; ++x)
+            {
+                const Pixel &p = this->buffer[y * width + x];
+                oss << "\033[38;2;" << (unsigned int)p.r << ";" << (unsigned int)p.g << ";" << (unsigned int)p.b << "m" << "██";
+            }
+            oss << "\n";
+        }
+        std::cout << oss.str();
+    }
 
-void clearScreen()
-{
-    std::cout << "\033[2J";
-}
+    // ! BUFFER OPS
+    void resetBuffer(Pixel p)
+    {
+        for (int j = 0; j < this->height; ++j)
+        {
+            for (int i = 0; i < this->width; ++i)
+            {
+                this->buffer[j * this->width + i] = p;
+            }
+        }
+    }
 
-void resetCursor()
-{
-    std::cout << "\033[H";
-}
+    void putPixel(int x, int y, Pixel color)
+    {
+        this->buffer[y * this->width + x] = color;
+    }
+
+    void swapBuffers()
+    {
+        std::memcpy(this->screen, this->buffer, sizeof(Pixel) * this->width * this->height);
+    }
+
+    // ! TERMINAL OPS
+    void clearScreen()
+    {
+        std::cout << "\033[2J";
+    }
+
+    void resetCursor()
+    {
+        std::cout << "\033[H";
+    }
+
+    // ! SHAPES
+    void rectangle(int x, int y, float rect_width, float rect_height, Pixel color)
+    {
+        for (int j = y; j < y + rect_height; ++j)
+        {
+            for (int i = x; i < x + rect_width; ++i)
+            {
+                this->putPixel(i, j, color);
+            }
+        }
+    }
+
+    void line(int x0, int y0, int x1, int y1, Pixel color)
+    {
+        int dx = abs(x1 - x0);
+        int dy = abs(y1 - y0);
+
+        int sx = (x0 < x1) ? 1 : -1;
+        int sy = (y0 < y1) ? 1 : -1;
+
+        int err = dx - dy;
+
+        while (true)
+        {
+            this->putPixel(x0, y0, color); // Set pixel at (x0, y0)
+
+            if (x0 == x1 && y0 == y1)
+                break; // Line has been drawn
+
+            int e2 = 2 * err;
+
+            if (e2 > -dy)
+            {
+                err -= dy;
+                x0 += sx;
+            }
+
+            if (e2 < dx)
+            {
+                err += dx;
+                y0 += sy;
+            }
+        }
+    }
+
+    void circleBorder(int centerX, int centerY, int radius, Pixel color)
+    {
+        int x = 0;
+        int y = radius;
+        int d = 1 - radius; // Decision variable
+
+        auto putSymmetricPixels = [&](int x, int y)
+        {
+            this->putPixel(centerX + x, centerY + y, color); // Octants
+            this->putPixel(centerX - x, centerY + y, color);
+            this->putPixel(centerX + x, centerY - y, color);
+            this->putPixel(centerX - x, centerY - y, color);
+            this->putPixel(centerX + y, centerY + x, color);
+            this->putPixel(centerX - y, centerY + x, color);
+            this->putPixel(centerX + y, centerY - x, color);
+            this->putPixel(centerX - y, centerY - x, color);
+        };
+
+        while (x <= y)
+        {
+            putSymmetricPixels(x, y);
+
+            if (d < 0)
+            {
+                d += 2 * x + 3;
+            }
+            else
+            {
+                d += 2 * (x - y) + 5;
+                y--;
+            }
+            x++;
+        }
+    }
+
+    void circleFill(int centerX, int centerY, int radius, Pixel color)
+    {
+        int x = 0;
+        int y = radius;
+        int d = 1 - radius;
+
+        auto putHorizontalLine = [&](int x, int y)
+        {
+            for (int i = -x; i <= x; i++)
+            {
+                this->putPixel(centerX + i, centerY + y, color);
+                this->putPixel(centerX + i, centerY - y, color);
+            }
+        };
+
+        while (x <= y)
+        {
+            putHorizontalLine(x, y);
+            putHorizontalLine(y, x);
+
+            if (d < 0)
+            {
+                d += 2 * x + 3;
+            }
+            else
+            {
+                d += 2 * (x - y) + 5;
+                y--;
+            }
+            x++;
+        }
+    }
+
+    // ! TEST LOOP
+    void loop()
+    {
+        this->clearScreen();
+        this->resetCursor();
+
+        float DELAY_uS = (1 / 60.0f) * 1000000;
+
+        float angle = 0;
+        float radius = 25;
+        int Ox = this->width / 2;
+        int Oy = this->height / 2;
+
+        while (true)
+        {
+            this->resetBuffer(Pixel{255, 255, 255});
+
+            // * DRAWING CODE GOES HERE --------------------------------------->
+
+            this->circleFill(Ox, Oy, 30, {0, 255, 0});
+            this->circleBorder(Ox, Oy, 30, {0, 0, 0});
+            this->line(Ox, Oy, Ox + radius * std::cos(angle), Oy + radius * std::sin(angle), {255, 0, 0});
+            this->rectangle(Ox, Oy, 1, 1, {0, 0, 255});
+
+            angle += 0.1;
+
+            if (angle >= 360)
+            {
+                angle = 0;
+            }
+
+            //*---------------------------------------------------------------->
+            this->swapBuffers();
+
+            this->resetCursor();
+            this->render();
+
+            usleep(DELAY_uS);
+        }
+    }
+};
 
 //*-----------------------
 //* UTILS
@@ -96,182 +279,12 @@ Pixel HSLtoPixel(int hue, float s, float l)
     return {col.r, col.g, col.b};
 }
 
-//*-----------------------
-//* SHAPES
-//*-----------------------
-
-void drawRectangle(Pixel *buffer, int width, int x, int y, float rect_width, float rect_height, Pixel p)
-{
-    for (int j = y; j < y + rect_height; ++j)
-    {
-        for (int i = x; i < x + rect_width; ++i)
-        {
-            putPixel(buffer, width, i, j, p);
-        }
-    }
-}
-
-void drawLine(Pixel *buffer, int width, int x0, int y0, int x1, int y1, Pixel color)
-{
-    int dx = abs(x1 - x0);
-    int dy = abs(y1 - y0);
-
-    int sx = (x0 < x1) ? 1 : -1;
-    int sy = (y0 < y1) ? 1 : -1;
-
-    int err = dx - dy;
-
-    while (true)
-    {
-        putPixel(buffer, width, x0, y0, color); // Set pixel at (x0, y0)
-
-        if (x0 == x1 && y0 == y1)
-            break; // Line has been drawn
-
-        int e2 = 2 * err;
-
-        if (e2 > -dy)
-        {
-            err -= dy;
-            x0 += sx;
-        }
-
-        if (e2 < dx)
-        {
-            err += dx;
-            y0 += sy;
-        }
-    }
-}
-
-void drawCircle(Pixel *buffer, int width, int centerX, int centerY, int radius, Pixel color)
-{
-    int x = 0;
-    int y = radius;
-    int d = 1 - radius; // Decision variable
-
-    auto putSymmetricPixels = [&](int x, int y)
-    {
-        putPixel(buffer, width, centerX + x, centerY + y, color); // Octants
-        putPixel(buffer, width, centerX - x, centerY + y, color);
-        putPixel(buffer, width, centerX + x, centerY - y, color);
-        putPixel(buffer, width, centerX - x, centerY - y, color);
-        putPixel(buffer, width, centerX + y, centerY + x, color);
-        putPixel(buffer, width, centerX - y, centerY + x, color);
-        putPixel(buffer, width, centerX + y, centerY - x, color);
-        putPixel(buffer, width, centerX - y, centerY - x, color);
-    };
-
-    while (x <= y)
-    {
-        putSymmetricPixels(x, y);
-
-        if (d < 0)
-        {
-            d += 2 * x + 3;
-        }
-        else
-        {
-            d += 2 * (x - y) + 5;
-            y--;
-        }
-        x++;
-    }
-}
-
-void fillCircle(Pixel *buffer, int width, int centerX, int centerY, int radius, Pixel color)
-{
-    int x = 0;
-    int y = radius;
-    int d = 1 - radius;
-
-    auto putHorizontalLine = [&](int x, int y)
-    {
-        for (int i = -x; i <= x; i++)
-        {
-            putPixel(buffer, width, centerX + i, centerY + y, color);
-            putPixel(buffer, width, centerX + i, centerY - y, color);
-        }
-    };
-
-    while (x <= y)
-    {
-        putHorizontalLine(x, y);
-        putHorizontalLine(y, x);
-
-        if (d < 0)
-        {
-            d += 2 * x + 3;
-        }
-        else
-        {
-            d += 2 * (x - y) + 5;
-            y--;
-        }
-        x++;
-    }
-}
-
 int main()
 {
     // ! SEEDING
     std::srand(static_cast<unsigned int>(std::time(nullptr)));
-
-    int screenWidth = 128;
-    int screenHeight = 128;
-    std::size_t frameCount = 0;
-
-    Pixel *screen = new Pixel[screenWidth * screenHeight];
-    Pixel *buffer = new Pixel[screenWidth * screenHeight];
-
-    clearScreen();
-    resetCursor();
-
-    float DELAY_uS = (1 / 60.0f) * 1000000;
-
-    float angle = 0;
-    float radius = 25;
-    int Ox = screenWidth / 2;
-    int Oy = screenHeight / 2;
-
-    while (true)
-    {
-        ++frameCount;
-        resetBuffer(buffer, screenWidth, screenHeight, Pixel{255, 255, 255});
-
-        // * DRAWING CODE GOES HERE --------------------------------------->
-
-        // for (int _ = 0; _ < 10; _++)
-        // {
-        //     drawRectangle(buffer, screenWidth, randomInteger(0, screenWidth - 10), randomInteger(0, screenHeight - 10), 10, 10, HSLtoPixel(randomInteger(0, 360), 1., 0.5));
-        // }
-
-        fillCircle(buffer, screenWidth, Ox, Oy, 30, {0, 255, 0});
-        drawCircle(buffer, screenWidth, Ox, Oy, 30, {0, 0, 0});
-        drawLine(buffer, screenWidth, Ox, Oy, Ox + radius * std::cos(angle), Oy + radius * std::sin(angle), {255, 0, 0});
-        drawRectangle(buffer, screenWidth, Ox, Oy, 1, 1, {0, 0, 255});
-
-        angle += 0.1;
-
-        if (angle >= 360)
-        {
-            angle = 0;
-        }
-
-        //*---------------------------------------------------------------->
-
-        std::memcpy(screen, buffer, sizeof(Pixel) * screenWidth * screenHeight);
-
-        resetCursor();
-        renderBuffer(buffer, screenWidth, screenHeight);
-
-        usleep(DELAY_uS);
-
-        // break;
-    }
-
-    delete[] buffer;
-    delete[] screen;
+    Renderer r{64, 64};
+    r.loop();
 
     return EXIT_SUCCESS;
 }
