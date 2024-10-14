48d19b9735eb0fcc3c0789f5088e2d440160356d
Muhammed Rajab <muhammedrajab101@gmail.com>
2024-10-14 16:58:28 +0530

    shit's loookign lit, ngl

--- main.cpp
+++ main.cpp
@@ -12,6 +12,14 @@
 // * MATH
 // *-------------------------------------->
 
+struct NormalColor
+{
+    float r;
+    float g;
+    float b;
+    float a;
+};
+
 float clamp(float min, float max, float val)
 {
     // return std::max(min, std::min(max, val));
@@ -67,6 +75,15 @@ struct Vec3
         };
     }
 
+    Vec3 scale(Vec3 b)
+    {
+        return {
+            this->x * b.x,
+            this->y * b.y,
+            this->z * b.z,
+        };
+    }
+
     float magnitude()
     {
         return std::sqrt(this->x * this->x + this->y * this->y + this->z * this->z);
@@ -80,9 +97,54 @@ struct Vec3
         this->z /= mag;
     }
 
+    Vec3 abs()
+    {
+        return {
+            std::fabs(this->x),
+            std::fabs(this->y),
+            std::fabs(this->z),
+        };
+    }
+
     float dot(Vec3 b)
     {
         return this->x * b.x + this->y * b.y + this->z * b.z;
+    }
+
+    Vec3 cos()
+    {
+        return {
+            std::cos(this->x),
+            std::cos(this->y),
+            std::cos(this->z),
+        };
+    }
+
+    Vec3 min(float val)
+    {
+        return {
+            std::min(this->x, val),
+            std::min(this->y, val),
+            std::min(this->z, val),
+        };
+    }
+
+    Vec3 max(float val)
+    {
+        return {
+            std::max(this->x, val),
+            std::max(this->y, val),
+            std::max(this->z, val),
+        };
+    }
+
+    Vec3 fract()
+    {
+        return {
+            this->x - std::floor(this->x),
+            this->y - std::floor(this->y),
+            this->z - std::floor(this->z),
+        };
     }
 
     Vec3 copy()
@@ -97,6 +159,16 @@ struct Vec3
             clamp(0, 255, this->y),
             clamp(0, 255, this->z),
             clamp(0, 255, alpha),
+        };
+    }
+
+    NormalColor toNormalColor(float alpha)
+    {
+        return {
+            clamp(0.0f, 1.0f, this->x),
+            clamp(0.0f, 1.0f, this->y),
+            clamp(0.0f, 1.0f, this->z),
+            clamp(0.0f, 1.0f, alpha),
         };
     }
 };
@@ -116,27 +188,70 @@ Pixel normalColorToPixel(float r, float g, float b, float a)
     };
 }
 
-struct NormalColor
+float smoothstep(float edge0, float edge1, float x)
 {
-    float r;
-    float g;
-    float b;
-    float a;
-};
+    x = clamp(0.0f, 1.0f, (x - edge0) / (edge1 - edge0));
+    return x * x * (3 - 2 * x);
+}
+
+float step(float threshold, float val)
+{
+    if (val <= threshold)
+    {
+        return 0;
+    }
+    else
+    {
+        return 1;
+    }
+}
+
+float fract(float val)
+{
+    return val - std::floor(val);
+}
+
+//*------------------------------------------->
+//* SDFs
+//*------------------------------------------->
+float sphere(Vec3 p, float r)
+{
+    return p.magnitude() - r;
+}
+
+float box(Vec3 p, Vec3 b)
+{
+    Vec3 q = p.abs().sub(b);
+    return q.max(0.0f).magnitude() + std::min(std::max(q.x, std::max(q.y, q.z)), 0.0f);
+}
+
+//*------------------------------------------>
+//* COLORS
+//*------------------------------------------>
+Vec3 color(float t, Vec3 a, Vec3 b, Vec3 c, Vec3 d)
+{
+    // C scales B elementwise
+    return a.add(b.scale(c.scale(t).add(d).scale(2 * M_PI).cos()));
+}
 
 int main()
 {
     // ! SEEDING
     std::srand(static_cast<unsigned int>(std::time(nullptr)));
 
-    // Renderer r{96, 96};
-    Renderer r{200, 96};
+    Renderer r{96, 96};
+    // Renderer r{64, 64};
+    // Renderer r{32, 32};
+    // Renderer r{200, 96};
+    // Renderer r{220, 220};
 
     r.clearScreen();
     r.resetCursor();
 
     // IN MILLISECONDS
-    int DELAY = 0;
+    int DELAY = 16;
+
+    std::size_t frameCount = 0;
 
     while (true)
     {
@@ -155,9 +270,40 @@ int main()
                 uv = uv.sub({0.5f, 0.5f, 0.0f}).scale(2.0f);
                 uv.x *= r.width / r.height;
 
-                float d = uv.magnitude();
+                Vec3 uv0 = uv.copy();
 
-                fragColor = {d, 0.0f, 0.0f, 1.0f};
+                Vec3 fColor{0.0f};
+                for (int i = 0; i < 3; ++i)
+                {
+
+                    uv = uv.scale(1.61803399f).fract().sub({0.5f, 0.5f});
+
+                    float d = sphere(uv, 0.5f);
+                    // float d1 = sphere(uv.sub({0.5f}), 0.5f);
+                    // float d2 = box(uv.sub({-0.25f}), {0.5f, 0.5f, 0.5f});
+
+                    // float d = std::min(d1, d2);
+
+                    Vec3 col = color(uv0.magnitude() + frameCount * 0.001f + i * 0.1,
+                                     {0.5f, 0.5f, 0.5f},
+                                     {0.5f, 0.5f, 0.5f},
+                                     {1.0f, 1.0f, 1.0f},
+                                     {0.00f, 0.10f, 0.20f});
+
+                    // Vec3 col{1.0f, 0.5f, 2.0f};
+                    d = std::sin(d * 8.0f + frameCount * 0.05f) / 8.0f;
+                    d = std::fabs(d);
+
+                    d = 0.009f / d;
+                    d = std::pow(d, 1.2f);
+                    // d = 0.01f / d;
+
+                    fColor = fColor.add(col.scale(d));
+                    // col = col.scale(d);
+                }
+
+                fragColor = fColor.toNormalColor(1.0f);
+                // fragColor = uv.toNormalColor(1.0f);
                 //*--------------------------------------->
                 r.putPixel(x, y, normalColorToPixel(fragColor.r, fragColor.g, fragColor.b, fragColor.a));
             }
@@ -169,6 +315,10 @@ int main()
         r.render();
 
         std::this_thread::sleep_for(std::chrono::milliseconds(DELAY)); // Control main loop delay
+
+        ++frameCount;
+
+        // return 0;
     }
 
     return EXIT_SUCCESS;
